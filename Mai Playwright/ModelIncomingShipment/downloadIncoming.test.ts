53f077d0a5693186c57408a6dfb3d1b0bbec1972
QuyenNha <chenhaquyen@gmail.com>
2023-09-11 22:35:52 +0700

    Update downloading

--- Mai Playwright/ModelIncomingShipment/downloadIncoming.test.ts
+++ Mai Playwright/ModelIncomingShipment/downloadIncoming.test.ts
@@ -4,15 +4,6 @@ import { test, expect, Page } from '@playwright/test';
 import { moveIncoming } from './moveIncoming.test';
 
 test.describe('Incoming_shipment_Download', () => {
-    //Download list
-    test('Download_list', async ({ page }) => {
-        //Move to: Incoming Shipment
-        await moveIncoming(page);
-        await page.locator("//button[text()='Export']").click();
-        expect(page.getByText("//button[text()='Export']"), "Download success");
-        await page.waitForTimeout(3000);
-    });
-
     //Download file upload
     test('Download_file upload', async ({ page }) => {
         //Move to: Incoming Shipment
@@ -22,6 +13,15 @@ test.describe('Incoming_shipment_Download', () => {
         await page.waitForTimeout(3000);
         (await page.locator("(//div[@class='file']//button)[1]")).click();
         expect(page.getByText("(//div[@class='file']//button)[1]"), "Download success");
+        await page.waitForTimeout(3000);
+    });
+
+    //Download list
+    test('Download_list', async ({ page }) => {
+        //Move to: Incoming Shipment
+        await moveIncoming(page);
+        await page.locator("//button[text()='Export']").click();
+        expect(page.getByText("//button[text()='Export']"), "Download success");
         await page.waitForTimeout(3000);
     });
 });
